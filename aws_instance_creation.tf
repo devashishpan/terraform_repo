2e6d0bf21c32c4de4de4a2a4442272f2cf8c7cc8
dk1998 <57136627+devashishpan@users.noreply.github.com>
2021-05-13 21:43:51 +0530

    Create aws_instance_creation.tf

---
+++ aws_instance_creation.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web_server"{
+  ami = "ami-010aff33ed5991201"
+  instance_type = "t2.micro"
+  security_group = ["web_server"]
+  key_name = "ssh-key-name"
+  tags = {
+    Name = "Webserver"
+  }
+}
